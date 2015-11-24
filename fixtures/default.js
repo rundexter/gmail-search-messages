c291f1bd7500b0a0b289cc3359fc1a1ac10445f3
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-24 17:37:02 +0200

    refactore

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,7 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                userId: 'al.zaretskii@gmail.com',
+                userId: 'gmail@gmail.com',
                 includeSpamTrash: false,
                 q: 'SQS'
             }
